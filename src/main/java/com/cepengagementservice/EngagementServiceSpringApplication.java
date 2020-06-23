c68297329f46a15134ed933ce4185a5699c6f821
Justice Gonda <5923472-gondajustice@users.noreply.gitlab.com>
2020-06-23 08:32:01 -0700

    initial spring-boot-app

---
+++ src/main/java/com/cepengagementservice/EngagementServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cepengagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngagementServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngagementServiceSpringApplication.class, args);
+	}
+
+}
