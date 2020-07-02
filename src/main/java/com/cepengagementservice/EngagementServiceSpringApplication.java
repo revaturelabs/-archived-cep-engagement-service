722b1652ed1a42afe91f860280dc37a421b6fff7
Lance Edwards <se.le7204@gmail.com>
2020-07-02 11:25:46 -0400

    added partial, switching workspaces

--- src/main/java/com/cepengagementservice/EngagementServiceSpringApplication.java
+++ src/main/java/com/cepengagementservice/EngagementServiceSpringApplication.java
@@ -1,13 +1,38 @@
 package com.cepengagementservice;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EngagementServiceSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EngagementServiceSpringApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper getObjectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES);
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		return mapper;
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
 	}
 
 }
