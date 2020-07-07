d472f333399c87010841c95be834d4226c13cd50
user <ggregar@gmail.com>
2020-07-07 00:17:05 -0500

    Added UserBatch test, UserBatchServiceTest, modified & for || in UserBatchService.

--- src/test/java/com/cepengagementservice/UserBatchTest/UserBatchTest.java
+++ src/test/java/com/cepengagementservice/UserBatchTest/UserBatchTest.java
@@ -1,5 +1,19 @@
 package com.cepengagementservice.UserBatchTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import com.cepengagementservice.Models.UserBatch;
+
+import org.junit.jupiter.api.Test;
+
 public class UserBatchTest {
-    
+
+    @Test
+    void createUserBatch(){
+        UserBatch test = new UserBatch(1, "last");
+        assertNotNull(test, "The constructor UserBatch must create an object");
+
+    }
+
 }
