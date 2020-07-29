8561624d284bbdc6fa143b0f904daa32fac7cdf6
Armondo Gomez <armondolgomez@gmail.com>
2020-07-29 15:21:15 -0500

    JDoc Repository

--- src/main/java/com/cepengagementservice/Config/CorsConfig.java
+++ src/main/java/com/cepengagementservice/Config/CorsConfig.java
@@ -5,19 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * WebMvcConfigurer takes no parameters which overrides aaCorsMapping to take in
- * a CorsRegistry as a parameter. The registry then added GET POST PUT and DELETE methods
- * Allows all headers and allow origins from localhost:3000
- * @author Unknown
- *
- */
+
 @Configuration
 public class CorsConfig {
 
 	/**
-	 * 
-	 * @return
+	 * WebMvcConfigurer takes no parameters which overrides aaCorsMapping to take in
+	 * a CorsRegistry as a parameter. The registry then added GET POST PUT and DELETE methods
+	 * Allows all headers and allow origins from localhost:3000
+	 * @author Unknown
+	 *
 	 */
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
