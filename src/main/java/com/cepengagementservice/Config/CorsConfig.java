e52c5d2478f01aa0775b258dcc3349fc69b70278
Armondo Gomez <armondolgomez@gmail.com>
2020-07-29 15:24:18 -0500

    JDoc Repository

--- src/main/java/com/cepengagementservice/Config/CorsConfig.java
+++ src/main/java/com/cepengagementservice/Config/CorsConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+/**
+ * configurations for the CORS issues
+ * Has WebMvcConfigurere and within that is addCorsMapping
+ * @author Unknown
+ *
+ */
 @Configuration
 public class CorsConfig {
 
