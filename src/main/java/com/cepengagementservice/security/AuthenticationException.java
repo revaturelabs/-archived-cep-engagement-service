8e15f2b1c841d9f9d56d8c82de959e5a4d85e2d0
Michael Worrell <michaelcourtlandw@gmail.com>
2020-07-29 12:21:43 -0400

    Implemented JDoc for AuthenticationConfig and AuthenticationException

--- src/main/java/com/cepengagementservice/security/AuthenticationException.java
+++ src/main/java/com/cepengagementservice/security/AuthenticationException.java
@@ -2,7 +2,10 @@ package com.cepengagementservice.security;
 
 //Use to throw invalid credentials or token exception @author Nicholas Larkin
 
-
+/**
+ * @author Unknown
+ * This will throw an RuntimeException when an invalid credentials or token appears
+ */
 public class AuthenticationException extends RuntimeException {   
 	
 private static final long serialVersionUID = 813478829857683389L; //generated
