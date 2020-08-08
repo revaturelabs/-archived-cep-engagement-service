ee873f5a0eca2e45b61d3489200086abc5ec837f
Simon Nardos <simonnardos@MacBook-Pro.local>
2020-08-07 23:04:03 -0600

    getting dev trev

--- Servers/registerService/src/test/java/com/register/controller/PendingUserControllerTest.java
+++ Servers/registerService/src/test/java/com/register/controller/PendingUserControllerTest.java
@@ -1,18 +1,40 @@
 package com.register.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.register.model.PendingUser;
 import com.register.service.PendingUserServiceImpl;
+
 
 @WebMvcTest(controllers = PendingUserController.class)
 @ActiveProfiles("test")
@@ -24,17 +46,66 @@ public class PendingUserControllerTest {
 	private PendingUserServiceImpl pendingUserService;
 	
 	private List<PendingUser> users;
+	
+	@Mock
+    private RestTemplate restTemplate;
+	
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@BeforeEach
 	void setUp() {
 		this.users = new ArrayList<>();
 		this.users.add(new PendingUser(1, "test", "test", "test", "test", "test", "test", "test"));
 		this.users.add(new PendingUser(2, "test1", "test1", "test1", "test1", "test1", "test1", "test1"));
+		
+		
+		
 	}
 	
 	@Test
-	void shouldFetchAllPendingUsers() {
+	void shouldFetchAllPendingUsers() throws Exception{
+		given(pendingUserService.allPendingUsers()).willReturn(users);
 		
+		this.mockMvc.perform(get("/pending/all"))
+		.andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()", is(users.size())));
 	}
+	
+	@Test
+	void testAddUser() throws Exception{
+		
+		PendingUser user = new PendingUser(3, "test3", "test3", "test3", "test3", "test3", "test3", "test3");
+		
+		HttpHeaders headers = new HttpHeaders();
+
+		// set Content-Type and Accept headers
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+		// example of custom header
+		headers.set("Authorization", "pass");
+		
+		HttpEntity<?> request = new HttpEntity<>(headers);
+		
+		String[] str = {};
+		
+		ResponseEntity<String[]> res = new ResponseEntity<String[]>(str, HttpStatus.OK);
+		
+		System.out.println(0);
+		
+		Mockito.when(restTemplate.exchange(ArgumentMatchers.eq("http://localhost:9015/users/email/all"), ArgumentMatchers.eq(HttpMethod.GET),
+				ArgumentMatchers.eq(request), ArgumentMatchers.eq(String[].class))).thenReturn(res);
+		
+		System.out.println(1);
+		
+		this.mockMvc.perform(post("/pending/add")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(user))
+				.characterEncoding("utf-8"))
+				.andExpect(status().isOk());
+				
+	}
+	
 
 }
---
+++ Servers/registerService/src/test/java/com/register/service/PendingUserServiceImplTest.java
@@ -0,0 +1,32 @@
+//package com.register.service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.register.model.PendingUser;
+//import com.register.repo.PendingUserRepo;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class PendingUserServiceImplTest {
+//
+//	
+//	@Mock
+//    private PendingUserRepo repo;
+//
+//    @InjectMocks
+//    private PendingUserServiceImpl userService;
+//    
+//    
+//    
+//    @Test
+//    void testAddUser() {
+//    	PendingUser user = new PendingUser(1, "test", "test", "test", "test", "test", "test", "test");
+//    	
+//    	given(this.repo.)
+//    }
+//    
+//}
