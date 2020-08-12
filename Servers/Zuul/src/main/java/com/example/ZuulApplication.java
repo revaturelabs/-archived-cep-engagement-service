7551108d938e40966481420d69e80ef3b7368c22
AnthonyNGarcia <antnoegar@gmail.com>
2020-08-11 17:33:11 -0700

    configured CorsFilter bean in Zuul to allowed * origin and made CrossOrigin for the ZuulApplication to accept origin from frontend EC2 ip address

--- Servers/Zuul/src/main/java/com/example/ZuulApplication.java
+++ Servers/Zuul/src/main/java/com/example/ZuulApplication.java
@@ -16,31 +16,31 @@ import org.springframework.web.filter.CorsFilter;
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
-@CrossOrigin(origins="*",  maxAge=3600, allowCredentials="true", allowedHeaders="*")
+@CrossOrigin(origins="http://ec2-3-229-134-85.compute-1.amazonaws.com",  maxAge=3600, allowCredentials="true", allowedHeaders="*")
 public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 	
-//	@Bean
-//	public CorsFilter corsFilter() {
-//	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//	    final CorsConfiguration config = new CorsConfiguration();
-//	    List<String> allowedOrigins = new ArrayList<>();
-//	    allowedOrigins.add("http://ec2-3-229-134-85.compute-1.amazonaws.com");
-//	    config.setAllowCredentials(true);
-//	    config.setAllowedOrigins(allowedOrigins);
-//	    config.addAllowedHeader("*");
-//	    config.addAllowedMethod("OPTIONS");
-//	    config.addAllowedMethod("HEAD");
-//	    config.addAllowedMethod("GET");
-//	    config.addAllowedMethod("PUT");
-//	    config.addAllowedMethod("POST");
-//	    config.addAllowedMethod("DELETE");
-//	    config.addAllowedMethod("PATCH");
-//	    source.registerCorsConfiguration("/**", config);
-//	    return new CorsFilter(source);
-//	}
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    List<String> allowedOrigins = new ArrayList<>();
+	    allowedOrigins.add("*");
+	    config.setAllowCredentials(true);
+	    config.setAllowedOrigins(allowedOrigins);
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("HEAD");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
+	    config.addAllowedMethod("PATCH");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
 
 }
